d2f8cc7bbcb2c7e70c89e7e0901bb96319e5de64
Siddharth Lele <siddharth@dzinesunlimited.com>
2017-08-06 19:47:06 +0530

    This is the first commit to publish the ZenPets source

--- Users/src/main/java/biz/zenpets/users/profile/questions/modules/MyPrivateQuestions.java
+++ Users/src/main/java/biz/zenpets/users/profile/questions/modules/MyPrivateQuestions.java
@@ -1,6 +1,41 @@
 package biz.zenpets.users.profile.questions.modules;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import biz.zenpets.users.R;
+import butterknife.ButterKnife;
 
 public class MyPrivateQuestions extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /** CAST THE LAYOUT TO A NEW VIEW INSTANCE **/
+        View view = inflater.inflate(R.layout.my_private_questions, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        /** INDICATE THAT THE FRAGMENT SHOULD RETAIN IT'S STATE **/
+        setRetainInstance(true);
+
+        /** INDICATE THAT THE FRAGMENT HAS AN OPTIONS MENU **/
+        setHasOptionsMenu(true);
+
+        /** INVALIDATE THE EARLIER OPTIONS MENU SET IN OTHER FRAGMENTS / ACTIVITIES **/
+        getActivity().invalidateOptionsMenu();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
